9546c02af281993fb35f34ce1a1191c3f61b7e6f
deivananderson <deyvananderson@hotmail.com>
2017-04-05 09:03:49 -0500

    Iniciando

---
+++ Geolocation/app/src/main/java/com/example/usuario/geolocation/AddressResultListener.java
@@ -0,0 +1,10 @@
+package com.example.usuario.geolocation;
+
+/**
+ * Created by USUARIO on 04/04/2017.
+ */
+
+public interface AddressResultListener {
+    void onAddressFound(String address);
+
+}
